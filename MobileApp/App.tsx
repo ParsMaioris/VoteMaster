ce81e8804792c6407fe5f6cc5188e9b6770a216e
InnovateWithRK <rk.go@icloud.com>
2024-08-07 04:22:47 -0700

    feat: set status bar to dark content for improved visibility

--- MobileApp/App.tsx
+++ MobileApp/App.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Navigation from './Infra/Navigation'
 import {Provider} from 'react-redux'
 import store from './Redux/Store'
+import {StatusBar} from 'react-native'
 
 export default function App()
 {
   return <Provider store={store}>
+    <StatusBar barStyle="dark-content" />
     <Navigation />
   </Provider>
 }
