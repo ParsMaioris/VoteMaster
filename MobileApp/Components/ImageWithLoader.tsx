0b35346bc2175c3ab3fcc22c0474d937454f1282
InnovateWithRK <rk.go@icloud.com>
2024-08-08 17:56:40 -0700

    refactor: Simplify ImageWithLoader component by removing loading animation and gradient background

--- MobileApp/Components/ImageWithLoader.tsx
+++ MobileApp/Components/ImageWithLoader.tsx
@@ -15,47 +15,14 @@ interface ImageWithLoaderProps
 
 const ImageWithLoader: React.FC<ImageWithLoaderProps> = ({source, style}) =>
 {
-    const dispatch = useDispatch<AppDispatch>()
-    const sourceKey = JSON.stringify(source)
-    const isLoaded = useSelector((state: RootState) => state.imageLoader.loadedImages[sourceKey])
-    const [loading, setLoading] = useState(!isLoaded)
-
-    useEffect(() =>
-    {
-        if (!isLoaded)
-        {
-            const timer = setTimeout(() =>
-            {
-                setLoading(false)
-                dispatch(markImageAsLoaded({source: sourceKey}))
-            }, 2000)
-            return () => clearTimeout(timer)
-        }
-    }, [isLoaded, dispatch, sourceKey])
-
     return (
         <View style={[styles.container, style]}>
-            {loading && (
-                <LinearGradient
-                    colors={['#d1ebff', '#97bce0']}
-                    style={styles.gradientBackground}
-                >
-                    <LottieView
-                        source={require('../assets/loading-animation.json')}
-                        autoPlay
-                        loop
-                        style={styles.lottie}
-                    />
-                </LinearGradient>
-            )}
-            {!loading && (
-                <Animatable.Image
-                    animation="fadeIn"
-                    duration={2000}
-                    source={source}
-                    style={style}
-                />
-            )}
+            <Animatable.Image
+                animation="fadeIn"
+                duration={2000}
+                source={source}
+                style={style}
+            />
         </View>
     )
 }
